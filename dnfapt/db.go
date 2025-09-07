346a62629a1e22abc3c6c2353e2aeefd1db3c3d5
abelgacem <amarbelgacem@hotmail.fr>
2025-09-07 14:49:47 +0200

    update

--- dnfapt/db.go
+++ dnfapt/db.go
@@ -3,13 +3,13 @@ package dnfapt
 var MapDaRepoReference = MapDaRepo{
 	"crio": {
 		Name:    "crio",
-		UrlRepo: "https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$TAG/$PACK/",
-		UrlGpg:  "https://download.opensuse.org/repositories/isv:/cri-o:/stable:/$TAG/$PACK/$GPG",
+		UrlRepo: "https://download.opensuse.org/repositories/isv:/cri-o:/stable:/v$TAG/$PACK/",
+		UrlGpg:  "https://download.opensuse.org/repositories/isv:/cri-o:/stable:/v$TAG/$PACK/$GPG",
 	},
 	"k8s": {
 		Name:    "k8s",
-		UrlRepo: "https://pkgs.k8s.io/core:/stable:/$TAG/$PACK/",
-		UrlGpg:  "https://pkgs.k8s.io/core:/stable:/$TAG/$PACK/$GPG",
+		UrlRepo: "https://pkgs.k8s.io/core:/stable:/v$TAG/$PACK/",
+		UrlGpg:  "https://pkgs.k8s.io/core:/stable:/v$TAG/$PACK/$GPG",
 	},
 }
 
--- filex/url.go
+++ filex/url.go
@@ -2,24 +2,28 @@ package filex
 
 import (
 	"fmt"
+	"strings"
 )
 
-func GetFileFromUrl(url string, filePath string, isRootPath bool) string{
+func CreateFileFromUrl(url string, filePath string, isRootPath bool) string {
 	sudoPrefix := ""
 	if isRootPath {
 		sudoPrefix = "sudo "
 	}
 
 	var cmds = []string{
-  	fmt.Sprintf(`curl -fsSL %s |  %stee %s`, url, sudoPrefix, filePath)
+		fmt.Sprintf(`curl -fsSL %s |  %stee %s`, url, sudoPrefix, filePath),
 	}
 	cli := strings.Join(cmds, " && ")
 	return cli
 }
 
-func GetGpgFileFromUrlAsSudo(url string, filePath string) string{
+func CreateGpgFileFromUrlAsSudo(url string, filePath string) string {
 	var cmds = []string{
-  	fmt.Sprintf(`curl -fsSL %s | gpg --dearmor | sudo tee %s`, url, filePath),
+		// fmt.Sprintf(`sudo install -d -m 0755  $(dirname %s)`, filePath),
+		"set -o pipefail",
+		fmt.Sprintf(`curl -fsSL %s | gpg --dearmor | sudo tee %s`, url, filePath),
+		fmt.Sprintf(`sudo chmod 0644  %s`, filePath),
 	}
 	cli := strings.Join(cmds, " && ")
 	return cli
