276ddbb253fc44885a02e5730c3684365e81c6b5
abelgacem <amarbelgacem@hotmail.fr>
2025-11-04 15:04:12 +0100

    update

---
+++ mock/node/node.go
@@ -0,0 +1,21 @@
+package node
+
+func IsSshConfigured() bool {
+	return true
+}
+
+func IsSshReachable() bool {
+	return true
+}
+
+func IsVm() bool {
+	return true
+}
+
+func IsLocal() bool {
+	return true
+}
+
+func IsContainer() bool {
+	return true
+}
