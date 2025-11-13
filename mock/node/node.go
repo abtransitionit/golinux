d15a58884328b8e286bf12382347a9aa5563e700
abelgacem <amarbelgacem@hotmail.fr>
2025-11-13 16:49:40 +0100

    update

--- mock/node/node.go
+++ mock/node/node.go
@@ -8,19 +8,23 @@ import (
 	"github.com/abtransitionit/golinux/mock/run"
 )
 
-func IsSshReachable(nodeName string, logger logx.Logger) (bool, error) {
+// Description: check a ssh target is reachable
+//
+// Notes:
+// - a ssh target is the localhost, a remote VM or a container
+func IsSshReachable(targetName string, logger logx.Logger) (bool, error) {
 
 	// 1 - Check node is configured.
-	isConfigured, err := IsSshConfigured(nodeName, logger)
+	isConfigured, err := IsSshConfigured(targetName, logger)
 	if err != nil {
-		return false, fmt.Errorf("checking ssh configuration")
+		return false, fmt.Errorf("checking ssh configuration: %w", err)
 	}
 	if !isConfigured {
 		return false, nil
 	}
 
 	// 2 -  Now that we know the node is configured, check if it's reachable.
-	cli := fmt.Sprintf("ssh -o BatchMode=yes -o ConnectTimeout=5 %s 'exit'", nodeName)
+	cli := fmt.Sprintf("ssh -o BatchMode=yes -o ConnectTimeout=5 %s 'exit'", targetName)
 
 	_, err = run.RunOnLocal(cli, logger)
 	if err != nil {
@@ -33,10 +37,14 @@ func IsSshReachable(nodeName string, logger logx.Logger) (bool, error) {
 
 }
 
-// Description: check if a VM is SSH configured.
-func IsSshConfigured(vmName string, logger logx.Logger) (bool, error) {
+// Description: check a ssh target is configured.
+//
+// Notes:
+// - a ssh target is the localhost, a remote VM or a container
+
+func IsSshConfigured(targetName string, logger logx.Logger) (bool, error) {
 	// build CLI command
-	cli := fmt.Sprintf("ssh -G %s | grep '^hostname ' | cut -d' ' -f2", vmName)
+	cli := fmt.Sprintf("ssh -G %s | grep '^hostname ' | cut -d' ' -f2", targetName)
 
 	// run command
 	resolvedHostname, err := run.RunOnLocal(cli, logger)
@@ -46,22 +54,29 @@ func IsSshConfigured(vmName string, logger logx.Logger) (bool, error) {
 
 	// trim spaces/newlines
 	resolvedHostname = strings.TrimSpace(resolvedHostname)
-	// logger.Debugf("resolvedHostname for %s > %q", vmName, resolvedHostname)
+	// logger.Debugf("resolvedHostname for %s > %q", targetName, resolvedHostname)
 
 	// logic: if hostname is empty or identical, it's not configured
-	isConfigured := resolvedHostname != "" && resolvedHostname != vmName
+	isConfigured := resolvedHostname != "" && resolvedHostname != targetName
 
 	return isConfigured, nil
 }
 
-func IsVm() bool {
+func IsRemoteVm() bool {
 	return true
 }
 
-func IsLocal() bool {
+func IsLocalVm() bool {
+	return true
+}
+
+func IsLocalHost() bool {
 	return true
 }
 
 func IsContainer() bool {
+	return true
+}
+func IsRemoteContainer() bool {
 	return true
 }
