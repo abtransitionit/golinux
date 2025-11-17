dbd286fbb2c53a9ec6bdab950cd15fb9b2071fe6
abelgacem <amarbelgacem@hotmail.fr>
2025-11-17 16:01:20 +0100

    update

---
+++ mock/onpm/README.md
@@ -0,0 +1,3 @@
+# Terminology
+
+- onpm: **O**s **N**ative **P**ackage **M**anager
---
+++ mock/onpm/onpm.go
@@ -0,0 +1,14 @@
+package onpm
+
+import (
+	"github.com/abtransitionit/gocore/logx"
+)
+
+func UpgradeOs(logger logx.Logger) (string, error) {
+	logger.Info("os upgrated")
+	return "", nil
+}
+func UpdateOs(logger logx.Logger) (string, error) {
+	logger.Info("os updated")
+	return "", nil
+}
