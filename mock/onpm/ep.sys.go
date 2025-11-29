d9142732ff805a8cc17b6353d7893def5e76a696
abelgacem <amarbelgacem@hotmail.fr>
2025-11-29 08:01:26 +0300

    update

--- mock/onpm/ep.sys.go
+++ mock/onpm/ep.sys.go
@@ -64,33 +64,33 @@ func UpgradeOs(hostName string, logger logx.Logger) (string, error) {
 func UpdateOs(hostName string, logger logx.Logger) (string, error) {
 	var osFamily, osDistro string
 	var err error
-	// 1 - get host:property
-	osFamily, err = property.GetProperty(logger, hostName, "osFamily")
-	if err != nil {
-		return "", err
-	}
-	osDistro, err = property.GetProperty(logger, hostName, "osDistro")
-	if err != nil {
-		return "", err
-	}
+	// // 1 - get host:property
+	// osFamily, err = property.GetProperty(logger, hostName, "osFamily")
+	// if err != nil {
+	// 	return "", err
+	// }
+	// osDistro, err = property.GetProperty(logger, hostName, "osDistro")
+	// if err != nil {
+	// 	return "", err
+	// }
 	// osKVersion, err := property.GetProperty(logger, hostName, "osKernelVersion")
 	// if err != nil {
 	// 	return "", err
 	// }
 
-	// if hostName == "o1u" {
-	// 	osFamily = "debian"
-	// 	osDistro = "ubuntu"
-	// } else if hostName == "o2a" {
-	// 	osFamily = "rhel"
-	// 	osDistro = "almalinux"
-	// } else if hostName == "o3r" {
-	// 	osFamily = "rhel"
-	// 	osDistro = "rocky"
-	// } else if hostName == "o4f" {
-	// 	osFamily = "fedora"
-	// 	osDistro = "fedora"
-	// }
+	if hostName == "o1u" {
+		osFamily = "debian"
+		osDistro = "ubuntu"
+	} else if hostName == "o2a" {
+		osFamily = "rhel"
+		osDistro = "almalinux"
+	} else if hostName == "o3r" {
+		osFamily = "rhel"
+		osDistro = "rocky"
+	} else if hostName == "o4f" {
+		osFamily = "fedora"
+		osDistro = "fedora"
+	}
 
 	// 2 - get a system manager
 	sysMgr, err := GetSysMgr(osFamily, osDistro)
